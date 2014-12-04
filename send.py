db5164c3be6100e6fcec259c8a424a0fcd2b3a3f
Scott Sadler <scott@alphanode.co>
2014-12-04 16:27:39 +0100

    some code

---
+++ send.py
@@ -0,0 +1,16 @@
+import sys
+import hashlib
+from ecdsa import SigningKey
+import verne.api
+
+# branch
+branch = verne.api.find_branch(sys.argv[1])
+
+# get address
+sk = SigningKey.from_pem(open('~/.gitcoin').read())
+vk = sk.get_verifying_key()
+address = hashlib.sha256(vk.to_der()).to_digest()
+
+# get balance
+import pdb; pdb.set_trace()
+1
