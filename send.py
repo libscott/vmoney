0160dc87d005281db10d6637f778b35a9c7e782e
Scott Sadler <scott@alphanode.co>
2014-12-04 16:49:40 +0100

    bump

--- send.py
+++ send.py
@@ -1,18 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python
 
-import sysgitb
+import sys
 import hashlib
 from ecdsa import SigningKey
 import verne.api
+import os.path
+
+
+gitcoinfile = os.path.expanduser('~/.gitcoin')
 
 # branch
 branch = verne.api.find_branch(sys.argv[1])
 
 # get address
-sk = SigningKey.from_pem(open('~/.gitcoin').read())
+sk = SigningKey.from_pem(open(gitcoinfile).read())
 vk = sk.get_verifying_key()
-address = hashlib.sha256(vk.to_der()).to_digest()
+address = hashlib.sha256(vk.to_der()).hexdigest()
 
 # get balance
-import pdb; pdb.set_trace()
-1
+inputs = []
+for entry in branch.tree:
+    if entry.name.startswith('balances/%s/' % address):
+       inputs.append(json.loads(branch[entry.name]))
+balance = sum(i['amount'] for i in inputs)
+print balance
